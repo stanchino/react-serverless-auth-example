4b23f9c802b11bf32d58ae23bb4a62cd3a3e4dde
Stanimir Dimitrov <stanchino@gmail.com>
2018-02-11 18:16:26 +0200

    Update: Add resend confirmation code button and refactored the application code

--- src/components/tests/Public.test.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import { Public } from "..";
-import { testComponent } from "./shared-examples";
-
-describe("Public", () => testComponent(<Public />));
--- src/components/tests/RouteWithRedirect.test.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import { RouteWithRedirect } from "..";
-import { testConnectedContainer } from "./shared-examples";
-
-describe("RouteWithRedirect", () => testConnectedContainer(<RouteWithRedirect path={'/auth/login'} to={'/'} />));
